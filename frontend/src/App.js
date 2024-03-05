5ed5a24c736ef43af58fe403dd11ea0948dfbbd3
Minh Kha <112142675+minhhhkha@users.noreply.github.com>
2024-03-05 15:26:19 +0700

    react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Shoes Fashion</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
